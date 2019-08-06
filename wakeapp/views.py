9474dbb1a0c11af56a4db4f60f8c427f7455f528
Jannik Schäfer <jannik.schaefer@inbox-online.de>
2019-08-06 13:50:26 +0200

    Add dummy data to sqlite DB and improve dashboard

--- wakeapp/views.py
+++ wakeapp/views.py
@@ -1,41 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from wakeapp import hosts
-
-computers_sample = list()
-categories_sample = list()
-
-category_pyhsical = hosts.Category("Physical Computers")
-category_vm = hosts.Category("Virtual Machines")
-categories_sample.extend([category_pyhsical, category_vm])
-
-computers_sample.append(hosts.Host("PC 1", category_pyhsical,
-                                   mac_address="00:00:00:00:00:00",
-                                   ipv4_address="192.168.0.1",
-                                   ipv6_address="2001:0db8:85a3:0000:0000:8a2e:0370:7334"))
-computers_sample.append(hosts.Host("PC 2", category_pyhsical))
-computers_sample.append(hosts.Host("PC 3", category_pyhsical))
-computers_sample.append(hosts.Host("PC 4", category_pyhsical))
-
-computers_sample.append(hosts.Host("VM 1", category_vm))
-computers_sample.append(hosts.Host("VM 2", category_vm))
+from .models import Host, HostCategory
 
 
 def home(request):
-    context = {'computers': computers_sample, 'categories': categories_sample,
-               'title': "Dashboard"}
+    context = {'hosts': Host.objects.all(), 'title': "Dashboard"}
+    print(context)
+    for key, val in context.items():
+        if key == 'hosts':
+            for elem in val:
+                try:
+                    print(elem.category)
+                except AttributeError:
+                    pass
     return render(request, 'wakeapp/home.html', context)
 
 
 def settings(request):
-    context = {'computers': computers_sample, 'categories': categories_sample,
-               'title': "Settings"}
+    context = {'hosts': Host.objects.all(), 'title': "Settings"}
     return render(request, 'wakeapp/settings.html', context)
 
 
 def testwake(request):
-    for host in computers_sample:
+    for host in Host.objects.all():
         try:
             host.start()
         except AttributeError:
