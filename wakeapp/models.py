42e80dc02d3a89ead5b88ae5ec11168731bde3b8
Jannik Schäfer <jannik.schaefer@inbox-online.de>
2019-08-06 12:35:10 +0200

    Implement Host and HostCategory models

--- wakeapp/models.py
+++ wakeapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from macaddress.fields import MACAddressField
+
 
 # Create your models here.
+
+class HostCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+
+class Host(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    ipv4_address = models.GenericIPAddressField(null=True, blank=True,
+                                                protocol='IPv4')
+    ipv6_address = models.GenericIPAddressField(null=True, blank=True,
+                                                protocol='IPv6')
+    mac_address = MACAddressField(null=True, blank=True)
+    wol_port = models.PositiveIntegerField(default=9)
+    state = models.BooleanField(default=False)
+    last_online = models.DateTimeField(null=True, blank=True)
+    date_added = models.DateTimeField(default=timezone.now)
+    date_edited = models.DateTimeField(default=timezone.now)
+    remote_vnc_url = models.URLField(null=True, blank=True)
+    remote_teamviewer_id = models.PositiveIntegerField(null=True, blank=True)
+    remote_splahstop_url = models.URLField(null=True, blank=True)
+    category = models.ForeignKey(HostCategory, on_delete=models.SET_NULL,
+                                 null=True, blank=True)
