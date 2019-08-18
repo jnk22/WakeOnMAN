05f701e94df2b206a73ec1e8c8cc48c9b61293a6
Jannik Schäfer <jannik.schaefer@inbox-online.de>
2019-08-18 16:08:17 +0200

    Implement error reducer

--- wakeonman/frontend/src/reducers/index.js
+++ wakeonman/frontend/src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
+import errors from './errors';
 import hosts from './hosts';
 import hostCategories from './hostcategories';
 
 export default combineReducers({
+    errors,
     hosts,
     hostCategories
 });
