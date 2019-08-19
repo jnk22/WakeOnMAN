8c53209cb36fae0e05c5af87d5a1a105a9833ea5
Jannik Schäfer <jannik.schaefer@inbox-online.de>
2019-08-19 20:18:30 +0200

    Revert previous changes

--- wakeonman/frontend/src/components/App.js
+++ wakeonman/frontend/src/components/App.js
@@ -1,31 +1,33 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from '../store';
 
 // Host and category components
 import Header from './Header';
 import Hosts from './Hosts';
+import HostFormAdd from './HostFormAdd';
 import HostCategories from './HostCategories';
+import HostCategoryFormAdd from './HostCategoryFormAdd';
 
 
 class App extends Component {
     render() {
         return (
             <Provider store={store}>
-                <Router>
-                    <div className="App">
-                        <Header/>
-                        <div className="container">
-                            <Switch>
-                                <Route path="/#hosts"
-                                       component={Hosts}/>
-                                <Route path="/#categories"
-                                       component={HostCategories}/>
-                            </Switch>
-                        </div>
+                <div className="App">
+                    <Header/>
+                    <div className="container">
+                        <br/>
+                        <Hosts/>
+                        <br/>
+                        <HostFormAdd/>
+                        <br/>
+                        <HostCategories/>
+                        <br/>
+                        <HostCategoryFormAdd/>
+                        <br/>
                     </div>
-                </Router>
+                </div>
             </Provider>
         );
     }
