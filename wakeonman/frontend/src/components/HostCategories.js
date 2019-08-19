26d72f2611c410bd9027ad1bbd2b056ccf12382c
Jannik Schäfer <jannik.schaefer@inbox-online.de>
2019-08-19 18:13:25 +0200

    Implement proper font color based on background contrast

--- wakeonman/frontend/src/components/HostCategories.js
+++ wakeonman/frontend/src/components/HostCategories.js
@@ -21,12 +21,50 @@ class HostCategories extends Component {
         this.props.getHostCategories();
     }
 
+    // Return color code for given category
     getCategoryColor = (category) => {
-        if (category) {
-            return {backgroundColor: category.color};
+        return category.color;
+    };
+
+    // Return font color (black/white) depending on given color contrast.
+    // useRelativeLuminance uses relative calculation as recommended by W3C.
+    getFontColor = (colorHex, useRelativeLuminance = true) => {
+        const color = {
+            r: parseInt(colorHex.substring(1, 3), 16),
+            g: parseInt(colorHex.substring(3, 5), 16),
+            b: parseInt(colorHex.substring(5, 7), 16)
+        };
+
+        if (useRelativeLuminance) {
+            // Return W3C compliant font color
+
+            for (const [key, value] of Object.entries(color)) {
+                let c = value / 255.0;
+                color[key] = (c <= 0.03928) ?
+                    (c / 12.92) : ((c + 0.055) / 1.055) ** 2.4;
+            }
+            const relativeLuminance = 0.2126 * color.r
+                + 0.7152 * color.g + 0.0722 * color.b;
+
+            return (relativeLuminance > 0.179) ? '#000000' : '#FFFFFF';
         } else {
-            return {backgroundColor: '#D3D3D3'};
+            // Return font color with simpler solution (not W3C compliant)
+
+            return ((color.r * 0.299 + color.g * 0.587 + color.b * 0.114) > 186) ?
+                '#000000' : '#FFFFFF';
         }
+    };
+
+    // Return CSS color styles for category background and set font to either
+    // white or black, depending on color
+    rowCategoryStyle = (category) => {
+        const backgroundColor = this.getCategoryColor(category);
+        const fontColor = this.getFontColor(backgroundColor);
+
+        return {
+            backgroundColor: backgroundColor,
+            color: fontColor
+        };
     };
 
     render() {
@@ -43,7 +81,7 @@ class HostCategories extends Component {
                     {this.props.hostCategories.length > 0 ? (
                         <tbody>
                         {this.props.hostCategories.map(category => (
-                            <tr style={this.getCategoryColor(category)}
+                            <tr style={this.rowCategoryStyle(category)}
                                 key={category.id}>
                                 <td>{category.name}</td>
                                 <td>
--- wakeonman/frontend/src/components/Hosts.js
+++ wakeonman/frontend/src/components/Hosts.js
@@ -45,15 +45,58 @@ class Hosts extends Component {
         }
     };
 
+    // Return color code based on host's category (if present), otherwise
+    // return default color code
     getCategoryColor = (host) => {
         const categoryObject = this.props.hostCategories.filter(category =>
             (category.id === host.category))[0];
 
         if (typeof (categoryObject) === 'object') {
-            return {backgroundColor: categoryObject.color};
+            return categoryObject.color;
         } else {
-            return {backgroundColor: '#D3D3D3'};
+            return '#D3D3D3';
         }
+    };
+
+    // Return font color (black/white) depending on given color contrast.
+    // useRelativeLuminance uses relative calculation as recommended by W3C.
+    getFontColor = (colorHex, useRelativeLuminance = true) => {
+        const color = {
+            r: parseInt(colorHex.substring(1, 3), 16),
+            g: parseInt(colorHex.substring(3, 5), 16),
+            b: parseInt(colorHex.substring(5, 7), 16)
+        };
+
+        if (useRelativeLuminance) {
+            // Return W3C compliant font color
+
+            for (const [key, value] of Object.entries(color)) {
+                let c = value / 255.0;
+                color[key] = (c <= 0.03928) ?
+                    (c / 12.92) : ((c + 0.055) / 1.055) ** 2.4;
+            }
+            const relativeLuminance = 0.2126 * color.r
+                + 0.7152 * color.g + 0.0722 * color.b;
+
+            return (relativeLuminance > 0.179) ? '#000000' : '#FFFFFF';
+        } else {
+            // Return font color with simpler solution (not W3C compliant)
+
+            return ((color.r * 0.299 + color.g * 0.587 + color.b * 0.114) > 186) ?
+                '#000000' : '#FFFFFF';
+        }
+    };
+
+    // Return CSS color styles for host background (based on host's category
+    // color code) and set font to either white or black, depending on color
+    rowHostStyle = (host) => {
+        const backgroundColor = this.getCategoryColor(host);
+        const fontColor = this.getFontColor(backgroundColor);
+
+        return {
+            backgroundColor: backgroundColor,
+            color: fontColor
+        };
     };
 
     render() {
@@ -71,7 +114,7 @@ class Hosts extends Component {
                     {this.props.hosts.length > 0 ? (
                         <tbody>
                         {this.props.hosts.map(host => (
-                            <tr style={this.getCategoryColor(host)}
+                            <tr style={this.rowHostStyle(host)}
                                 key={host.id}>
                                 <td>{this.getState(host)}{host.name}</td>
                                 <td>{this.getCategoryName(host)}</td>
